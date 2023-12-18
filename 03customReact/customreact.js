8f165b38325b7dfde9461b20de573b2f13cfef16
Molla1441 <mhasim1441@gmail.com>
2023-12-18 16:28:47 +0530

    make a counter project with hooks

--- 03customReact/customreact.js
+++ 03customReact/customreact.js
@@ -1,1 +1,20 @@
-const mainContainer = document.getElementById('root')
+function customRender(reactElement, Container) {
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+    Container.appendChild(domElement)
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Chick me to visit google",
+};
+
+const mainContainer = document.getElementById("root");
+
+customRender(reactElement,mainContainer)
