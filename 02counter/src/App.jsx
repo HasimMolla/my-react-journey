7064dc3ce4eed2b351f0bf746fe8b90049a9c574
Molla1441 <mhasim1441@gmail.com>
2023-12-19 23:47:16 +0530

    add useState hooks in my project for UI updation

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// problem is UI updation , this is control by React
+// React reacts on the updation of variables
+// that's why it was named React , it's a very reactive library
+// it reacts everwhere when any thing update
+//React and UI syncing
+// every hooks a individual hooks 
 
+function App() {
+
+  let [counter, setCounter] = useState(0)
+
+  // let counter = 15
+
+  const addValue = () => {
+    setCounter(counter + 1)
+    if (counter === 20) {
+      setCounter(20)
+    } 
+    console.log("clicked", counter);
+    
+
+  }
+  const removeValue = () => {
+    setCounter(counter - 1) 
+  if (counter === 0) {
+    setCounter(0)
+  }
+    console.log("clicked", counter);
+    
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Click the button to update counter value</h1>
+      <h2>counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <footer>footer: {counter}</footer>
     </>
   )
 }
