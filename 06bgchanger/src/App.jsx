cad46d011de9fbb5a1cc4411eae9d84e87075e59
Molla1441 <mhasim1441@gmail.com>
2024-01-08 11:19:20 +0530

    chage in style

--- 06bgchanger/src/App.jsx
+++ 06bgchanger/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
 function App() {
-  const [color, setColor] = useState("olive")
+  const [color, setColor] = useState("gray")
 
   return (
     <div className='w-full h-screen' style={{ backgroundColor: color }}>
       
-      <div className='fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-2'>
-        <div className='flex flex-wrap justify-center gap-3 bg-white px-3 py-2 rounded-3xl '>
+      <div className='fixed flex flex-wrap justify-center top-2 inset-x-0 px-2'>
+        <div className='flex flex-wrap shadow-lg justify-center gap-3 bg-white px-3 py-2 rounded-3xl '>
           <button onClick={()=>setColor("red")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"red"}}>Red</button>
           <button onClick={() => setColor("green")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"green"}}>Green</button>
           <button onClick={() => setColor("blue")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"blue"}}>Blue</button>
