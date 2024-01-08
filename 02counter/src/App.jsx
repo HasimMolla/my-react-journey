faae9dff3c3863f7727d0cd91fcce449bdba8cb0
Molla1441 <mhasim1441@gmail.com>
2024-01-08 11:09:34 +0530

    Done bgchanger project with hook

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,7 +10,7 @@ import './App.css'
 
 function App() {
 
-  let [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(0)
 
   // let counter = 15
 
---
+++ 06bgchanger/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div className='w-full h-screen' style={{ backgroundColor: color }}>
+      
+      <div className='fixed flex flex-wrap justify-center bottom-10 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 bg-white px-3 py-2 rounded-3xl '>
+          <button onClick={()=>setColor("red")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"red"}}>Red</button>
+          <button onClick={() => setColor("green")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"green"}}>Green</button>
+          <button onClick={() => setColor("blue")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"blue"}}>Blue</button>
+          <button onClick={() => setColor("olive")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"olive"}}>Olive</button>
+          <button onClick={() => setColor("gray")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"gray"}}>Gray</button>
+          <button onClick={() => setColor("yellow")} className='outline-none px-4 py-2 shadow-lg rounded-full text-black' style={{backgroundColor:"yellow"}}>Yellow</button>
+          <button onClick={() => setColor("pink")} className='outline-none px-4 py-2 shadow-lg rounded-full text-black' style={{backgroundColor:"pink"}}>Pink</button>
+          <button onClick={() => setColor("purple")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"purple"}}>Purple</button>
+          <button onClick={() => setColor("white")} className='outline-none px-4 py-2 shadow-lg rounded-full' style={{backgroundColor:"white"}}>White</button>
+          <button onClick={() => setColor("black")} className='outline-none px-4 py-2 shadow-lg rounded-full text-white' style={{backgroundColor:"black"}}>Black</button>
+          <button onClick={() => setColor("lavender")} className='outline-none px-4 py-2 shadow-lg rounded-full text-black' style={{backgroundColor:"lavender"}}>Lavender</button>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
