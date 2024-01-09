0488013145063bd4a2fc48562b3840637b218562
Molla1441 <mhasim1441@gmail.com>
2024-01-09 22:16:31 +0530

    copy fun done ✅ and useRef use

--- 07passwordGenerator/src/App.jsx
+++ 07passwordGenerator/src/App.jsx
@@ -1,10 +1,13 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
   const [length, setlength] = useState(8)
   const [noAllowed, setnoAllowed] = useState(false)
   const [charAllowed, setcharAllowed] = useState(false)
   const [password, setPassword] = useState("")
+
+  //useRef hook
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -22,15 +25,23 @@ function App() {
 
   }, [length, noAllowed, charAllowed.setPassword])
 
-  useEffect(() => {passwordGenerator() }, [length, noAllowed, charAllowed, passwordGenerator])
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 49);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  //useEffect hook
+  useEffect(() => { passwordGenerator() }, [length, noAllowed, charAllowed, passwordGenerator])
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto rounded-lg text-orange-500 shadow-lg p-4  my-8 bg-gray-700'>
+      <div className='w-full max-w-lg mx-auto rounded-lg text-orange-500 shadow-lg p-4  my-8 bg-gray-700'>
         <h1 className='text-2xl text-center text-white my-3'>Password Generator</h1>
         <div className='flex rounded-lg shadow-md  overflow-hidden mb-4'>
-          <input type="text" value={password} className='outline-none text-lg w-full py-2 px-3' placeholder='Password' readOnly />
-          <button className='outline-none bg-blue-600 text-white px-4 py-0.5 shrink-0 '>copy</button>
+          <input type="text" value={password} className='outline-none text-lg w-full py-2 px-3' placeholder='Password' readOnly ref={passwordRef} />
+          <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-500 font-bold text-white px-4 py-0.5 shrink-0 hover:bg-blue-800 hover:text-gray-300 '>copy</button>
         </div>
         <div className='flex text-md mt-5 gap-x-5'>
           <div className='flex item-center gap-x-1'>
