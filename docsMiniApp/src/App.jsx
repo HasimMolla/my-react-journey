4804462ccc915e88bfdeb46790ed83146d951695
Molla1441 <mhasim1441@gmail.com>
2024-01-11 00:18:50 +0530

    done bg components

--- docsMiniApp/src/App.jsx
+++ docsMiniApp/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Background from './components/Background'
 function App() {
   return (
     <div className='relative flex justify-center items-center w-full h-screen bg-zinc-800'>
-      <h1 className='absolute text-[12vw] font-semibold   text-zinc-900 tracking-tight leading-none'>Docs.</h1>
+      <Background/>
+      
     </div>
   )
 }
---
+++ docsMiniApp/src/components/Background.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Background() {
+  return (
+      <>
+          <div className='absolute top-[5%] w-full flex justify-center text-xl text-zinc-600 py-8'>Documents.</div>
+          <h1 className='absolute text-[12vw] font-semibold   text-zinc-900 tracking-tighter leading-none'>Docs.</h1>
+      </>
+  )
+}
+
+export default Background
