455c5573d2bb1c40b411d39521562825e2b68c99
Molla1441 <mhasim1441@gmail.com>
2024-01-10 23:58:24 +0530

    add docs mini app project

---
+++ docsMiniApp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='relative flex justify-center items-center w-full h-screen bg-zinc-800'>
+      <h1 className='absolute text-[12vw] font-semibold   text-zinc-900 tracking-tight leading-none'>Docs.</h1>
+    </div>
+  )
+}
+
+export default App
