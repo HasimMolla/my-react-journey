745dca425b4924cf5ac55bea47245cf8af2b0682
Molla1441 <mhasim1441@gmail.com>
2024-01-13 23:15:33 +0530

    dynamic data with props

--- docsMiniApp/src/components/Card.jsx
+++ docsMiniApp/src/components/Card.jsx
@@ -1,27 +1,33 @@
+/* eslint-disable react/prop-types */
 // import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
 import { motion } from "framer-motion"
-// import { IoClose } from "react-icons/io5";
-function Card({ reference, tagName = "Download Now", filesize = ".4mb", content ='This is a mini docs webApps, In future i will add a form where user add their data.' }) {
+import { IoClose } from "react-icons/io5";
+// eslint-disable-next-line react/prop-types
+function Card({ data, reference }) {
 
   return (
     <>
       <motion.div drag dragConstraints={reference} whileDrag={{ scale: 1.2 }} dragElastic={0.2} dragTransition={{ bounceStiffness: 600, bounceDamping: 4 }} className='relative overflow-hidden w-60 h-72 rounded-[35px] bg-zinc-900 text-white p-6 hover:cursor-grab'>
         <FaRegFileAlt />
-        <p className="font-base mt-[20px] leading-tight text-sm">{ content}</p>
+        <p className="font-base mt-[20px] leading-tight text-sm">{ data.decs}</p>
         <div className="footer absolute bottom-0 w-full left-0">
           <div className="flex item-center justify-between px-8 py-3 mb-3 ">
-            <h5 className="text-sm">{ filesize}</h5>
+            <h5 className="text-sm">{data.filesize}</h5>
             <span className="w-6 h-6 bg-zinc-500 rounded-full flex items-center justify-center">
-              <LuDownload size="0.8rem" />
+              {data.close ? <IoClose/> : <LuDownload size="0.8rem" /> }            
             </span>
-
           </div>
-          <div className="tag w-full py-3 bg-green-600 flex items-center justify-center hover:cursor-pointer">
-            <h3 className="text-sm font-semibold ">{ tagName}</h3>
+          {
+            data.tag.isOpen && (
+              <div className="tag w-full py-3 bg-green-600 flex items-center justify-center hover:cursor-pointer">
+                <h3 className="text-sm font-semibold ">Download now</h3>
 
-          </div>
+              </div>
+            ) 
+          }
+          
         </div>
       </motion.div>
     </>
--- docsMiniApp/src/components/Foreground.jsx
+++ docsMiniApp/src/components/Foreground.jsx
@@ -1,15 +1,26 @@
+/* eslint-disable react/jsx-key */
 // import React, { useState } from 'react'
 import { useRef } from 'react'
 import Card from './Card'
 
 function Foreground() {
+  const data = [{
+    decs: 'This is a mini docs webApps, In future i will add a form where user add their data.',
+    filesize: '.4mb',
+    close: true,
+    tag: { isOpen: true, tagTitle: "Download Now", tagColor: 'green' },
+  }];
   const ref = useRef(null)
   return (
     <>
       <div ref={ref} className='fixed z-[3] top-0 left-0 w-full h-screen p-5 flex flex-wrap gap-5 '>
-        <Card  reference={ref} tagName="open now" />
+        {/* <Card  reference={ref} tagName="open now" />
         <Card  reference={ref} filesize=".8mb" />
-        <Card reference={ref} filesize=".2mb"  content="This is a normal docs webApps but I will update day by day."/>
+        <Card reference={ref} filesize=".2mb"  content="This is a normal docs webApps but I will update day by day."/> */}
+        {data.map((item, index) => (
+          <Card data={item} reference={ref} />
+        ))}
+       
       </div>
     </>
   )
