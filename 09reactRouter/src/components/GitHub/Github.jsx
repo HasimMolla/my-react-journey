d46c1ab7bbe5a0f338d853c0076ba625645dee68
Molla1441 <mhasim1441@gmail.com>
2024-01-18 00:26:59 +0530

    add loader for fast data fetching with optimization

--- 09reactRouter/src/components/GitHub/Github.jsx
+++ 09reactRouter/src/components/GitHub/Github.jsx
@@ -1,21 +1,29 @@
-import  { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-     const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/hiteshchoudhary')
-            .then(response => response.json())
-            .then(data => {
-                setData(data)
-            })
-    }, [])
+    const data = useLoaderData()
+    //  const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/HasimMolla')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             setData(data)
+    //         })
+    // }, [])
+    
     return (
         <div className='text-center m-4 bg-gray-500 text-white p-4 text-3xl'>
             <div>Github followers: {data.followers}</div>
-            <img  className='h-[200px] w-[200px] object-cover mt-3' src={data.avatar_url} alt="" />
+            <img  className='h-[200px] w-[200px] object-cover mt-3' src={data.avatar_url} alt="Git Picture" />
 
         </div>
     )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/HasimMolla')
+    return response.json()
+}
