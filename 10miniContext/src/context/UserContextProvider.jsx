8f0e57728594da5ecd32de497473ee0669cb96eb
Molla1441 <mhasim1441@gmail.com>
2024-01-26 18:59:29 +0530

    setup our project

---
+++ 10miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserContext from "./Usercontext";
+
+const UserContextProvider = ({ children }) => {
+    const [user, setUser] = React.useState(null)
+    return (
+        <UserContext.Provider value={{user,setUser}}>
+          {children}   
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider;
+
