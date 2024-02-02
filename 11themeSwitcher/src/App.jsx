35d9453deae6840a8f3bebf03fa071e486ab389f
Molla1441 <mhasim1441@gmail.com>
2024-02-02 20:35:31 +0530

    core functionality done

--- 11themeSwitcher/src/App.jsx
+++ 11themeSwitcher/src/App.jsx
@@ -1,20 +1,40 @@
+import { useEffect, useState } from "react"
+import { ThemeProvider } from "./Contexts/theme"
 
 
 function App() {
 
+  const [themeMode, setThemeMode] = useState('light')
+
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  //actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('dark', 'light')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
 
   return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* themeBtn */}
-        </div>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* themeBtn */}
+          </div>
 
-        <div className="w-full max-w-sm mx-auto">
-          {/* Card */}
+          <div className="w-full max-w-sm mx-auto">
+            {/* Card */}
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 
