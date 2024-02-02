ed6f220a8d3cf8dc798c87c47a05e6a9ddc14fea
Molla1441 <mhasim1441@gmail.com>
2024-02-02 20:21:28 +0530

    add context

--- 10miniContext/src/components/Profile.jsx
+++ 10miniContext/src/components/Profile.jsx
@@ -1,12 +1,12 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import UserContext from '../context/Usercontext'
 
 function Profile() {
-  const {user} = useContext(UserContext)
+  const { user } = useContext(UserContext)
   if (!user) return <div className='text-red-400 font-mono font-medium text-xl'>Please Login!</div>
   return <div className='text-green-500 text-xl'>
-    Welcome <span className='text-white text-base '>{user.username}</span>
+    Welcome <span className='text-white text-lg '>{user.username}</span>
   </div>
-  
+
 }
-  export default Profile
+export default Profile
