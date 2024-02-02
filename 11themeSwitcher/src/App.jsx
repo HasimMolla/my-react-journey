1ccf28cd48a606171fe82759c44b1de49c44ca22
Molla1441 <mhasim1441@gmail.com>
2024-02-02 21:17:11 +0530

    done dark mode

--- 11themeSwitcher/src/App.jsx
+++ 11themeSwitcher/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import { ThemeProvider } from "./Contexts/theme"
+import ThemeBtn from "./Components/Themebtn"
+import Card from "./Components/Card"
 
 
 function App() {
@@ -26,11 +28,11 @@ function App() {
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* themeBtn */}
+            <ThemeBtn/>
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-            {/* Card */}
+            <Card/>
           </div>
         </div>
       </div>
