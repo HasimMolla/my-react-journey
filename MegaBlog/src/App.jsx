9e949ad51ea83c7e4570d4176ce1bafeb2ebde6a
Molla1441 <mhasim1441@gmail.com>
2024-03-11 11:35:59 +0530

    add: parallex zooming with scroll project

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
     <h1>A mega  project with appwrite</h1>
---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,7 @@
+import conf from "../conf/conf.js";
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService { }
+
+export default AuthService
